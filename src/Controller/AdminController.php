98b89bcb9b96b2811a5dadbd8b93515437cbb15c
jerome <louloubomont@hotmail.fr>
2022-10-10 19:38:05 +0200

    create menu controller and added a field in product on db

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -13,8 +13,6 @@ class AdminController extends AbstractController
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/index.html.twig', []);
     }
 }
